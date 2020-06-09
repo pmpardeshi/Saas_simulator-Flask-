208174cbaf631f6a40139ef55c90e6846b513f0d
pmpardeshi <pramodmpardeshi@gmail.com>
2020-06-09 10:49:53 +0530

    Encryption Added

--- cloud.py
+++ cloud.py
@@ -1,10 +1,17 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, send_file, flash
+import pyAesCrypt
+import shutil
+
 
 
 app= Flask(__name__)
 app.config['UPLOAD_FOLDER']='static'
 app.config['SECRET_KEY']='377959898262d5bd73edcd7992e8ca35'
+bufferSize = 64 * 1024
+password = "3edcp7992ezca3k"
+buf='temp'
+
 
 def get_files_info():
 	file_list={}
@@ -14,10 +21,14 @@ def get_files_info():
 	
 	for path,dirs,files in os.walk(start_path):
 		for file in files:
+			unit='MB'
 			f_path=os.path.join(path,file)
 			size=os.path.getsize(f_path)/1000000
 			total_size+=size
-			file_list[i]=[file,size]
+			if size<1:
+				size=round(size*1000,2)
+				unit='KB'
+			file_list[i]=[file,size,unit]
 			i+=1
 
 	total_size = round(total_size,2)
@@ -30,8 +41,11 @@ def get_files_info():
 def save_file():
 	if(request.method == 'POST'):
 		f=request.files['fl1']
-		f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+		f_path=os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
+		f.save(f_path)
+		pyAesCrypt.encryptFile(f_path, f"{f_path}.aes", password, bufferSize)
 		flash(f'{f.filename} is uploaded ',category='success')
+		os.remove(f_path)
 	return redirect(url_for('home'))
 	
 
@@ -45,8 +59,12 @@ def delete_file(fname):
 
 @app.route("/view_file/<int:task>/<string:fname>")
 def view_file(task,fname):
+	if not os.path.isdir(buf):
+		os.mkdir(buf)
 	f_path=os.path.join(app.config['UPLOAD_FOLDER'],fname)
-	return send_file(f_path,as_attachment=task)
+	buf_path=os.path.join(buf,fname[:-4])
+	pyAesCrypt.decryptFile(f_path,buf_path, password, bufferSize)
+	return send_file(buf_path,as_attachment=task)
 	
 	
 
@@ -54,6 +72,8 @@ def view_file(task,fname):
 @app.route("/")
 @app.route("/home")
 def home():
+	if os.path.isdir(buf):
+		shutil.rmtree(buf)
 	file_list,total_size,percent_full=get_files_info()
 	return render_template('display.html',file_list=file_list,total_size=total_size,percent_full=percent_full)
 
